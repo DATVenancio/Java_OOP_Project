f2c5a4f9e59798ce0cfef5d78d066c3632b52978
ines <ines02607@gmail.com>
2023-11-11 23:14:21 +0100

    modif_jusqua_choisir_personnage

--- core/src/com/mygdx/game/Menu.java
+++ core/src/com/mygdx/game/Menu.java
@@ -12,19 +12,34 @@ import com.badlogic.gdx.InputProcessor;
 
 public class Menu {
     private SpriteBatch batch;
+
+    // Texture des menus
     private Texture menu1Texture;
     private Texture menu2Texture;
     private Texture menu3Texture;
+
+    // Texture des règles du jeu
     private Texture regleTexture;
+
+    // Section Force
     private Texture choixForce;
+    private Texture ForceHomme;
+    private Texture ForceFemme;
+
+    // Section Dextérité
     private Texture choixDexterite;
+    private Texture DexteriteHomme;
+    private Texture DexteriteFemme;
+
+    // Section Intelligence
     private Texture choixIntelligence;
+    private Texture IntelligenceHomme;
+    private Texture IntelligenceFemme;
+
+    // Texture du moment pour les boucles
     private Texture currentTexture;
     private long startTime;
     private boolean showMenu1 = true;
-    private boolean cliqueBouton1 = false;
-    private boolean cliqueBouton2 = false;
-    private boolean cliqueBouton3 = false;
     private int page = 0;
 
     public Menu() {
@@ -37,7 +52,12 @@ public class Menu {
         choixForce = new Texture("choixforce.jpg");
         choixDexterite = new Texture("choixdexterite.jpg");
         choixIntelligence = new Texture("choixintelligence.jpg");
-
+        ForceHomme = new Texture("forcehomme.jpg");
+        ForceFemme = new Texture("forcefemme.jpg");
+        DexteriteHomme = new Texture("dexteritehomme.jpg");
+        DexteriteFemme = new Texture("dexteritefemme.jpg");
+        IntelligenceHomme = new Texture("intelligencehomme.jpg");
+        IntelligenceFemme = new Texture("intelligencefemme.jpg");
 
         startTime = TimeUtils.millis();
     }
@@ -87,28 +107,61 @@ public class Menu {
                         page = 6;
                     }
                 }
-                    cliqueBouton1 = false;
-                    cliqueBouton2 = false;
-                    cliqueBouton3 = false;
 
-                    return false;
+                if (currentTexture == choixForce || currentTexture == choixDexterite || currentTexture == choixIntelligence) {
+                    if (screenX >= 180 && screenX <= 380 && screenY >= 60 && screenY <= 135) {
+                        // bouton retour
+                        page = 1;
+                    }
+                    if (screenX >= 600 && screenX <= 1040 && screenY >= 240 && screenY <= 965) {
+                        if(currentTexture == choixForce){// bouton "Homme Force"
+                            page = 7;
+                        }
+                        if(currentTexture == choixDexterite){
+                            page = 9;
+                        }
+                        if(currentTexture == choixIntelligence){
+                            page = 11;
+                        }
+                    }
+                    if (screenX >= 1155 && screenX <= 1595 && screenY >= 240 && screenY <= 965) {
+                        // bouton "Femme Force"
+                        if(currentTexture == choixForce){// bouton "Homme Force"
+                            page = 8;
+                        }
+                        if(currentTexture == choixDexterite){
+                            page = 10;
+                        }
+                        if(currentTexture == choixIntelligence){
+                            page = 12;
+                        }
+                    }
+                }
+                if (currentTexture == ForceHomme || currentTexture == ForceFemme) {
+                    if (screenX >= 180 && screenX <= 380 && screenY >= 60 && screenY <= 135) {
+                        // bouton retour
+                        page = 4;
+                    }
+                }
+
+                if (currentTexture == DexteriteHomme || currentTexture == DexteriteFemme) {
+                    if (screenX >= 180 && screenX <= 380 && screenY >= 60 && screenY <= 135) {
+                        // bouton retour
+                        page = 5;
+                    }
+                }
+
+                if (currentTexture == IntelligenceHomme || currentTexture == IntelligenceFemme) {
+                    if (screenX >= 180 && screenX <= 380 && screenY >= 60 && screenY <= 135) {
+                        // bouton retour
+                        page = 6;
+                    }
+                }
+                return false;
             }
 
             @Override
             public boolean touchDown(int screenX, int screenY, int pointer, int bouton) {
-
-                if(screenX >= 485 && screenX <= 930 && screenY >= 360 && screenY <= 510)
-                {
-                    cliqueBouton1=true;
-                }
-                if(screenX >= 485 && screenX <= 930 && screenY >= 590 && screenY <= 740)
-                {
-                    cliqueBouton2=true;
-                }
-                if(screenX >= 485 && screenX <= 930 && screenY >= 830 && screenY <= 980)
-                {
-                    cliqueBouton3=true;
-                }
                 return false;
             }
 
@@ -210,6 +263,30 @@ public class Menu {
             batch.draw(choixIntelligence, x, y);
             currentTexture = choixIntelligence;
         }
+        if(page == 7)  {// si on est à la page Choix Intelligence
+            batch.draw(ForceHomme, x, y);
+            currentTexture = ForceHomme;
+        }
+        if(page == 8)  {// si on est à la page Choix Intelligence
+            batch.draw(ForceFemme, x, y);
+            currentTexture = ForceFemme;
+        }
+        if(page == 9)  {// si on est à la page Choix Intelligence
+            batch.draw(DexteriteHomme, x, y);
+            currentTexture = DexteriteHomme;
+        }
+        if(page == 10)  {// si on est à la page Choix Intelligence
+            batch.draw(DexteriteFemme, x, y);
+            currentTexture = DexteriteFemme;
+        }
+        if(page == 11)  {// si on est à la page Choix Intelligence
+            batch.draw(IntelligenceHomme, x, y);
+            currentTexture = IntelligenceHomme;
+        }
+        if(page == 12)  {// si on est à la page Choix Intelligence
+            batch.draw(IntelligenceFemme, x, y);
+            currentTexture = IntelligenceFemme;
+        }
 
         batch.end();
     }
@@ -238,6 +315,24 @@ public class Menu {
             case 6:
                 dessinerPage(6);
                 break;
+            case 7:
+                dessinerPage(7);
+                break;
+            case 8:
+                dessinerPage(8);
+                break;
+            case 9:
+                dessinerPage(9);
+                break;
+            case 10:
+                dessinerPage(10);
+                break;
+            case 11:
+                dessinerPage(11);
+                break;
+            case 12:
+                dessinerPage(12);
+                break;
         }
     }
 
@@ -254,6 +349,7 @@ public class Menu {
     }
 
     public void dispose() {
+        // pour la libération mémoire
         batch.dispose();
         menu1Texture.dispose();
         menu2Texture.dispose();
@@ -262,5 +358,11 @@ public class Menu {
         choixForce.dispose();
         choixDexterite.dispose();
         choixIntelligence.dispose();
+        ForceHomme.dispose();
+        ForceFemme.dispose();
+        DexteriteHomme.dispose();
+        DexteriteFemme.dispose();
+        IntelligenceHomme.dispose();
+        IntelligenceFemme.dispose();
     }
 }
