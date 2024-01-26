65dce58d0a9b7c33f18fd79a6dbbbe65ef64e265
DATVenancio <danielvenancioif@gmail.com>
2024-01-26 11:50:31 +0100

    Javadoc

---
+++ core/doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.mygdx.game.Controller"},{"l":"com.mygdx.game.Model"},{"l":"com.mygdx.game.View"}];updateSearchResults();
